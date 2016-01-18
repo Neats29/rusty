7bf5b18297b7f52637c7e76b65e78fe897f1fd6f
Anita Amini <anita.amini@cohaesus.co.uk>
2016-01-18 08:56:18 +0000

    guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,20 @@
+use std::io; //use is like import, io is the library from the standard library
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); //variables are immutable by default but mut makes them mutable.
+    //String::new() creates a new, empty String. 
+
+    io::stdin().read_line(&mut guess) // calls stdin which takes user input, then save the input to the guess variable, hence why it needs to be mutable. if immutable, we'd write &guess
+        .ok().expect("Failed to read line"); 
+        //for error handling. ok() and expect() are methods from the 'io::Result' sub-library. ok() lets us assume the program is 
+        //gonna be successful, if error, we just want a generic error not a specific one from Rsut in this case.
+        // expect(): if err, log the string.
+        
+
+    println!("You guessed: {}", guess);
+}
+
